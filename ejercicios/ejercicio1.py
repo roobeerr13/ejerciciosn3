09b3c3498140cfd9e4451cf9153c0ba35c9fd249
Roberto Jimenez <robertojj2006@gmail.com>
2025-04-03 10:24:25 +0200

    nuevo commit mas

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -9,11 +9,13 @@ def mover_hanoi(n, origen, auxiliar, destino):
 def mover_hanoi_resumen(n, origen, auxiliar, destino, contador, limite=10):
     if n == 1:
         contador[0] += 1
+        # Imprime solo si está dentro del rango de los primeros o últimos movimientos
         if contador[0] <= limite or contador[0] > (2**n - 1) - limite:
             print(f"Movimiento {contador[0]}: Mover piedra de {origen} a {destino}")
     else:
         mover_hanoi_resumen(n - 1, origen, destino, auxiliar, contador, limite)
         contador[0] += 1
+        # Imprime solo si está dentro del rango de los primeros o últimos movimientos
         if contador[0] <= limite or contador[0] > (2**n - 1) - limite:
             print(f"Movimiento {contador[0]}: Mover piedra de {origen} a {destino}")
         mover_hanoi_resumen(n - 1, auxiliar, origen, destino, contador, limite)
